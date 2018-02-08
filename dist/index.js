18cf9068da7397ab0d94ad3e5f073b936ca325bd
Piotr Falba <piotr@falba.net>
2018-02-08 09:42:21 +0100

    Few fixes

--- dist/index.js
+++ dist/index.js
@@ -1,36 +1,19 @@
-'use strict';
+import { useDeps as _useDeps } from './simpleDi';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.disable = exports.composeAll = exports.composeWithObservable = exports.composeWithPromise = exports.composeWithTracker = exports.compose = exports.useDeps = exports.createApp = undefined;
+import { compose as _compose, composeWithTracker as _composeWithTracker, composeWithPromise as _composeWithPromise, composeWithObservable as _composeWithObservable, composeAll as _composeAll, disable as _disable } from 'react-komposer';
 
-var _simpleDi = require('./simpleDi');
-
-var _reactKomposer = require('react-komposer');
-
-var _app = require('./app');
-
-var _app2 = _interopRequireDefault(_app);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+import App from './app';
 
 // export this module's functions
-var createApp = exports.createApp = function createApp() {
-  for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
-    args[_key] = arguments[_key];
-  }
-
-  return new (Function.prototype.bind.apply(_app2.default, [null].concat(args)))();
-};
+export const createApp = (...args) => new App(...args);
 
 // export react-simple-di functions
-var useDeps = exports.useDeps = _simpleDi.useDeps;
+export const useDeps = _useDeps;
 
 // export react-komposer functions
-var compose = exports.compose = _reactKomposer.compose;
-var composeWithTracker = exports.composeWithTracker = _reactKomposer.composeWithTracker;
-var composeWithPromise = exports.composeWithPromise = _reactKomposer.composeWithPromise;
-var composeWithObservable = exports.composeWithObservable = _reactKomposer.composeWithObservable;
-var composeAll = exports.composeAll = _reactKomposer.composeAll;
-var disable = exports.disable = _reactKomposer.disable;
+export const compose = _compose;
+export const composeWithTracker = _composeWithTracker;
+export const composeWithPromise = _composeWithPromise;
+export const composeWithObservable = _composeWithObservable;
+export const composeAll = _composeAll;
+export const disable = _disable;
--- dist/simpleDi.js
+++ dist/simpleDi.js
@@ -1,44 +1,18 @@
-'use strict';
+import _extends from 'babel-runtime/helpers/extends';
+import React from 'react';
+import hoistStatics from 'hoist-non-react-statics';
+import CreateReactClass from 'create-react-class';
+import PropTypes from 'prop-types';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+const getDisplayName = Component => Component.displayName || Component.name || 'Component';
 
-var _extends2 = require('babel-runtime/helpers/extends');
-
-var _extends3 = _interopRequireDefault(_extends2);
-
-exports.injectDeps = injectDeps;
-exports.useDeps = useDeps;
-
-var _react = require('react');
-
-var _react2 = _interopRequireDefault(_react);
-
-var _hoistNonReactStatics = require('hoist-non-react-statics');
-
-var _hoistNonReactStatics2 = _interopRequireDefault(_hoistNonReactStatics);
-
-var _propTypes = require('prop-types');
-
-var _propTypes2 = _interopRequireDefault(_propTypes);
-
-var _createReactClass = require('create-react-class');
-
-var _createReactClass2 = _interopRequireDefault(_createReactClass);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-var getDisplayName = function getDisplayName(Component) {
-  return Component.displayName || Component.name || 'Component';
-}; // eslint-disable-line no-unused-vars
-function injectDeps(context, _actions) {
-  var actions = {};
-  for (var key in _actions) {
+export function injectDeps(context, _actions) {
+  const actions = {};
+  for (let key in _actions) {
     if (_actions.hasOwnProperty(key)) {
-      var actionMap = _actions[key];
-      var newActionMap = {};
-      for (var actionName in actionMap) {
+      const actionMap = _actions[key];
+      const newActionMap = {};
+      for (let actionName in actionMap) {
         if (actionMap.hasOwnProperty(actionName)) {
           newActionMap[actionName] = actionMap[actionName].bind(null, context);
         }
@@ -48,67 +22,53 @@ function injectDeps(context, _actions) {
   }
 
   return function (Component) {
-    var ComponentWithDeps = (0, _createReactClass2.default)({
-      displayName: 'ComponentWithDeps',
-
+    const ComponentWithDeps = CreateReactClass({
       childContextTypes: {
-        context: _propTypes2.default.object,
-        actions: _propTypes2.default.object
+        context: PropTypes.object,
+        actions: PropTypes.object
       },
 
-      getChildContext: function getChildContext() {
+      getChildContext() {
         return {
-          context: context,
-          actions: actions
+          context,
+          actions
         };
       },
-      render: function render() {
-        return _react2.default.createElement(Component, this.props);
+
+      render() {
+        return React.createElement(Component, this.props);
       }
     });
 
-    ComponentWithDeps.displayName = 'WithDeps(' + getDisplayName(Component) + ')';
-    return (0, _hoistNonReactStatics2.default)(ComponentWithDeps, Component);
+    ComponentWithDeps.displayName = `WithDeps(${getDisplayName(Component)})`;
+    return hoistStatics(ComponentWithDeps, Component);
   };
 }
 
-var defaultMapper = function defaultMapper(_context, _actions2) {
-  return {
-    context: function context() {
-      return _context;
-    },
-    actions: function actions() {
-      return _actions2;
-    }
-  };
-};
+const defaultMapper = (context, actions) => ({
+  context: () => context,
+  actions: () => actions
+});
 
-function useDeps() {
-  var mapper = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : defaultMapper;
-
+export function useDeps(mapper = defaultMapper) {
   return function (Component) {
-    var ComponentUseDeps = (0, _createReactClass2.default)({
-      displayName: 'ComponentUseDeps',
-      render: function render() {
-        var _context2 = this.context,
-            context = _context2.context,
-            actions = _context2.actions;
+    const ComponentUseDeps = CreateReactClass({
+      render() {
+        const { context, actions } = this.context;
+        const mappedProps = mapper(context, actions);
 
-        var mappedProps = mapper(context, actions);
+        const newProps = _extends({}, this.props, mappedProps);
 
-        var newProps = (0, _extends3.default)({}, this.props, mappedProps);
-
-        return _react2.default.createElement(Component, newProps);
+        return React.createElement(Component, newProps);
       },
 
-
       contextTypes: {
-        context: _propTypes2.default.object,
-        actions: _propTypes2.default.object
+        context: PropTypes.object,
+        actions: PropTypes.object
       }
     });
 
-    ComponentUseDeps.displayName = 'UseDeps(' + getDisplayName(Component) + ')';
-    return (0, _hoistNonReactStatics2.default)(ComponentUseDeps, Component);
+    ComponentUseDeps.displayName = `UseDeps(${getDisplayName(Component)})`;
+    return hoistStatics(ComponentUseDeps, Component);
   };
 }
