22c296cccfbd57eb97ad6d0855986948c2771001
Jakub Kania <jakub@thinksmart.pl>
2018-09-14 14:48:16 +0200

    Dist

--- dist/composeWithTracker.js
+++ dist/composeWithTracker.js
@@ -7,7 +7,15 @@ exports.composeWithTracker = composeWithTracker;
 
 var _reactKomposer = require('react-komposer');
 
-function composeWithTracker(reactiveFn, L, E, options) {
+function composeWithTracker(reactiveFn) {
+  var L = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : function () {
+    return null;
+  };
+  var E = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : function () {
+    return null;
+  };
+  var options = arguments[3];
+
   var onPropsChange = function onPropsChange(props, onData, context) {
     var trackerCleanup = void 0;
     var handler = Tracker.nonreactive(function () {
